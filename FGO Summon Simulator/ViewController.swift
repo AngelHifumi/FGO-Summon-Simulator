81bafb2341e1e187989eb1437b01557cf1946586
Timur Sinnurow <42872306+AngelHifumi@users.noreply.github.com>
2020-10-19 16:52:12 +0200

    Update ViewController.swift

--- FGO Summon Simulator/ViewController.swift
+++ FGO Summon Simulator/ViewController.swift
@@ -7,9 +7,7 @@
 //
 
 import UIKit
-
 class ViewController: UIViewController {
-    
     @IBOutlet weak var showServants: UIButton!
     @IBOutlet weak var dolarwasted: UILabel!
     @IBOutlet weak var amountSR: UILabel!
@@ -24,6 +22,24 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
     }
+    func weightedRandomElement<T>(items: [(T, UInt)]) -> T {
+
+        let total = items.map { $0.1 }.reduce(0, +)
+    precondition(total > 0, "The sum of the weights must be positive")
+
+    let rand = UInt(arc4random_uniform(UInt32(total)))
+
+    var sum = UInt(0)
+    for (element, weight) in items {
+        sum += weight
+        if rand < sum {
+            return element
+        }
+    }
+
+    fatalError("This should never be reached")
+}
+    
     class Servant: NSCoder{
         
         var name: String = ""
@@ -63,6 +79,33 @@ class ViewController: UIViewController {
     func getThreeList() -> [Servant]{
         return threeStarReceived
     }
+    let list = [
+    Servant(name: "Squirturia",  rarity: 5, amount: 1, class_: "Archer"),
+    Servant(name: "Tamamo",  rarity: 5, amount: 1, class_: "Lancer"),
+    Servant(name: "Tamamo",  rarity: 5, amount: 1, class_: "Caster"),
+    Servant(name: "Jalter",  rarity: 5, amount: 1, class_: "Pog"),
+    Servant(name: "Rauikou",  rarity: 5, amount: 1, class_: "Berserker"),
+    Servant(name: "Loli Killer",  rarity: 5, amount: 1, class_: "Assassin"),
+    Servant(name: "Shishou",  rarity: 5, amount: 1, class_: "Lancer"),
+    Servant(name: "Skadi",  rarity: 5, amount: 1, class_: "Caster"),
+    Servant(name: "Merlin",  rarity: 5, amount: 1, class_: "Caster"),
+    Servant(name: "Salter",  rarity: 4, amount: 1, class_: "Saber"),
+    Servant(name: "Tamamo",  rarity: 4, amount: 1, class_: "Lancer"),
+    Servant(name: "Tamamo",  rarity: 4, amount: 1, class_: "Berserker"),
+    Servant(name: "Jalter",  rarity: 4, amount: 1, class_: "Berserker"),
+    Servant(name: "Ushiwakamaru",  rarity: 4, amount: 1, class_: "Assassin"),
+    Servant(name: "Marie Antoinette",  rarity: 4, amount: 1, class_: "Caster"),
+    Servant(name: "Marie Antoinette",  rarity: 4, amount: 1, class_: "Rider"),
+    Servant(name: "Boobie Arturia",  rarity: 4, amount: 1, class_: "Lancer"),
+    Servant(name: "Euryle",  rarity: 3, amount: 1, class_: "Archer"),
+    Servant(name: "Chu",  rarity: 3, amount: 1, class_: "Lancer"),
+    Servant(name: "Kiyohime",  rarity: 3, amount: 1, class_: "Lancer"),
+    Servant(name: "Casesar",  rarity: 3, amount: 1, class_: "Saber"),
+    Servant(name: "Hans Memerson",  rarity: 3, amount: 1, class_: "Caster"),
+    Servant(name: "Gil(Child)",  rarity: 3, amount: 1, class_: "Archer"),
+    Servant(name: "Ushiwakamaru",  rarity: 3, amount: 1, class_: "Rider"),
+    Servant(name: "David",  rarity: 3, amount: 1, class_: "Archer"),
+    ]
     let servSSRList = [
         Servant(name: "Squirturia",  rarity: 5, amount: 1, class_: "Archer"),
         Servant(name: "Tamamo",  rarity: 5, amount: 1, class_: "Lancer"),
@@ -95,12 +138,18 @@ class ViewController: UIViewController {
         Servant(name: "David",  rarity: 3, amount: 1, class_: "Archer"),
         
     ]
+    
     var SRamount = 0
     var SSRamount = 0
+    var amountOfRollsForSSRleft = 90
     func gacha() -> Servant{
+        
         var randomServant:Servant
                 
         let randomNumber = Int.random(in: 1...100)
+        if amountOfRollsForSSRleft == 1 {
+            return pity()
+        }
         if randomNumber <= 2{
             randomServant = servSSRList.randomElement()!
             SSRamount = SSRamount + 1
@@ -119,13 +168,29 @@ class ViewController: UIViewController {
             threeStarReceived.append(randomServant)
         }
         
+        amountOfRollsForSSRleft -= 1
+        print(amountOfRollsForSSRleft)
         return randomServant
         
     }
+    func pity () -> Servant{
+        var randomServant: Servant
+        randomServant = servSSRList.randomElement()!
+        SSRamount = SSRamount + 1
+        amountSSR.text = "Amount of SSRs: " + "\(SSRamount)"
+        ssrReceived.append(randomServant)
+        amountOfRollsForSSRleft = 90
+        return randomServant
+    }
+    
     var isEnough = true
     var bucks = 0
+    @IBAction func prepareListSSR(_ sender:
+           Any) {
+        let list = getSSRList()
+           performSegue(withIdentifier: "name", sender: self)
+       }
     @IBAction func addSQ(_ sender: Any) {
-    
     }
     @IBAction func summon(_ sender: Any) {
         let sq = Int(sqAmount.text!)!
@@ -142,8 +207,12 @@ class ViewController: UIViewController {
         receivedServant.text = servant.getName() + " " + servant.getClass()
         bucks = bucks + 3
         dolarwasted.text = "\(bucks)" + "$"
+        
     }
     
     
 }
+
+
+
 
